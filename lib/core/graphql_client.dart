6279780721b6de714c9ad3cd5fed7cd29e5c0d21
linnefromice <linnefromice@gmail.com>
2021-05-01 19:48:45 +0900

    create GraphQLClient wrapper - initialize client

---
+++ lib/core/graphql_client.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+import 'app_config.dart';
+
+class GraphQLApiClient {
+  GraphQLApiClient(final AppConfig config) {
+    graphQLClient = ValueNotifier<GraphQLClient>(
+      GraphQLClient(
+        cache: GraphQLCache(store: HiveStore()),
+        link: HttpLink(config.baseUrl),
+      )
+    );
+  }
+
+  ValueNotifier<GraphQLClient> graphQLClient;
+
+  Future<QueryResult> query(
+    String query, {
+      Map<String, dynamic> variables
+  }) async {
+    final result = await graphQLClient.value.query(QueryOptions(
+      document: gql(query),
+      variables: variables,
+    ));
+
+    if (result.exception != null) {
+      // エラー処理
+      print(result.exception);
+      for (final error in result.exception.graphqlErrors) {
+        print(error.message);
+      }
+      return result;
+    }
+  }
+}
