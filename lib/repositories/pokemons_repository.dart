74ef7668f06c23cb63a337dc59e55bc543907355
linnefromice <linnefromice@gmail.com>
2021-05-06 23:22:57 +0900

    update repository

--- lib/repositories/pokemons_repository.dart
+++ lib/repositories/pokemons_repository.dart
@@ -1,5 +1,7 @@
 import '../core/graphql_client.dart';
 import '../models/pokemons.dart';
+import '../models/pokemons_item.dart';
+import '../queries/query_pokeapi.dart';
 
 class PokemonsRepository {
   PokemonsRepository(this._client);
@@ -7,24 +9,30 @@ class PokemonsRepository {
   final GraphQLApiClient _client;
 
   Future<Pokemons> getPokemons() async {
-    final result = await _client.query(query, variables: {});
-    return Pokemons.fromJson(result.data["pokemons"]);
+    final result = await _client.queryDocumentNode(
+      PokemonsQuery().document,
+      variables: {}
+    );
+    // return Pokemons.fromJson(result.data["pokemons"]);
+    return _mapPokemons(Pokemons$Query.fromJson(result.data).pokemons);
   }
-}
 
-const String query = """
-query {
-  pokemons {
-    count
-    next
-    previous
-    status
-    message
-    results {
-      url
-      name
-      image
-    }
+  Pokemons _mapPokemons(Pokemons$Query$PokemonList item) {
+    return Pokemons(
+      count: item.count,
+      next: item.next,
+      previous: item.previous,
+      status: item.status,
+      message: item.message,
+      results: item.results.map(_mapPokemonsItem).toList()
+    );
+  }
+
+  PokemonsItem _mapPokemonsItem(Pokemons$Query$PokemonList$PokemonItem item) {
+    return PokemonsItem(
+      url: item.url,
+      name: item.name,
+      image: item.image
+    );
   }
 }
-  """;
